47ec39139ce25ab69e9ead43ce1cf173035f79bd
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-04-21 00:23:33 +0200

    web config

---
+++ src/main/java/informatyka/TLSv13/controller/Controller.java
@@ -0,0 +1,15 @@
+package informatyka.TLSv13.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+
+        return "To jest test";
+    }
+}
