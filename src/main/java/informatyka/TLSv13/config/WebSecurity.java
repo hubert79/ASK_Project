77886a8090d6b8a0166ae86904ac91796f2d8049
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-04-20 21:03:14 +0200

    sec

--- src/main/java/informatyka/TLSv13/config/WebSecurity.java
+++ src/main/java/informatyka/TLSv13/config/WebSecurity.java
@@ -2,8 +2,9 @@ package informatyka.TLSv13.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 }
